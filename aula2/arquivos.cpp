a7f8116eb23acc40b25e0a7ad1e76e6dd5eb12e3
Felkng <felipersf374@gmail.com>
2023-08-09 22:48:44 -0300

    gg

--- aula2/arquivos.cpp
+++ aula2/arquivos.cpp
@@ -8,10 +8,10 @@ using namespace std;
 
 class Arquivo{
 public:
-    int ***matrizTimes;
-    int ***matrizMachine;
-    int ***matrizResultado;
-
+    int **matrizTimes;
+    int **matrizMachine;
+    int **matrizResultado;
+    
 };
 
 int main(){
@@ -22,26 +22,21 @@ int main(){
     string linha;
     int aux=0; //contador de matrizes
     int entradas = 10; //quantidade de matrizes
-    string machines[entradas];
-    // string times[entradas];
     getline(ifs,primeiraLinha); // pega a primeira linha
     string num = "";
-    Arquivo arq;
+    Arquivo arq[entradas];
 
-    arq.matrizMachine = new int**[entradas];
-    arq.matrizTimes = new int**[entradas];
-    arq.matrizResultado = new int**[entradas];
 
-    for(int i=0; i<entradas; i++){
-        arq.matrizMachine[i] = new int*[nLinhas];
-        arq.matrizTimes[i] = new int*[nLinhas];
-        arq.matrizResultado[i] = new int*[nLinhas];
-        for(int j=0; j<nLinhas; j++){
-            arq.matrizMachine[i][j] = new int[nLinhas];
-            arq.matrizTimes[i][j] = new int[nLinhas];
-            arq.matrizResultado[i][j] = new int[nLinhas];
+    for (int i = 0; i < entradas; i++) {
+        arq[i].matrizMachine = new int*[nLinhas];
+        arq[i].matrizTimes = new int*[nLinhas];
+        arq[i].matrizResultado = new int*[nLinhas];
+        for (int j = 0; j < nLinhas; j++) {
+            arq[i].matrizMachine[j] = new int[nLinhas];
+            arq[i].matrizTimes[j] = new int[nLinhas];
+            arq[i].matrizResultado[j] = new int[nLinhas];
         }
-    }
+}
 
     while(getline(ifs,linha)) { // enquanto end of file for false continua
         if(linha == "Times"){
@@ -59,23 +54,22 @@ int main(){
                     }
                     else if(helper[j] == ' '  && num != ""){
                         istringstream(num) >> int_num;
-                        arq.matrizTimes[aux][i][col]  = int_num;
+                        arq[aux].matrizTimes[i][col]  = int_num;
                         col++;
                         num = "";
                     }
                     if(helper[j] == '\0'){ // se for o final da linha atribui o ultimo valor de num a matriz
                         istringstream(num) >> int_num;
-                        arq.matrizTimes[aux][i][col]  = int_num;
+                        arq[aux].matrizTimes[i][col]  = int_num;
                         num = "";
                     }
                 }
-                    for(int j=0; j<nLinhas; j++){
-                        cout << arq.matrizTimes[aux][i][j] << " ";
-                    }
-                    cout << endl;
+                // for(int j=0; j<nLinhas; j++){
+                //     cout << arq[aux].matrizTimes[i][j] << " ";
+                // }
+                // cout << endl;
                 }
-                cout << endl;
-
+                // cout << endl;
         }
         if(linha == "Machines"){
             for(int i=0; i<nLinhas;i++){
@@ -84,8 +78,6 @@ int main(){
                 strcpy(helper, linha.c_str());
                 int col=0;
                 int int_num;
-
-
                 for(int j=0; j<linha.length()+1; j++){
                     if(helper[j] != ' '){
                         num.push_back(helper[j]); //concatena os caracteres em uma string
@@ -93,29 +85,40 @@ int main(){
                     }
                     else if(helper[j] == ' '  && num != ""){
                         istringstream(num) >> int_num;
-                        arq.matrizMachine[aux][i][col]  = int_num;
+                        arq[aux].matrizMachine[i][col]  = int_num;
                         col++;
                         num = "";
                     }
                     if(helper[j] == '\0'){ // se for o final da linha atribui o ultimo valor de num a matriz
                         istringstream(num) >> int_num;
-                        arq.matrizMachine[aux][i][col]  = int_num;
+                        arq[aux].matrizMachine[i][col]  = int_num;
                         num = "";
                     }
                 }
-                    for(int j=0; j<nLinhas; j++){
-                        cout << arq.matrizMachine[aux][i][j] << " ";
-                    }
-                    cout << endl;
+                // for(int j=0; j<nLinhas; j++){
+                //     cout << arq[aux].matrizMachine[i][j] << " ";
+                // }
+                // cout << endl;
                 }
-                cout << endl;
-            if(linha == primeiraLinha){ // se a linha for igual a primeira linha passa para a proxima posição do vetor
-                aux+=1;
-                if(aux==entradas) //evita falha de segmentação
-                    break;
-            }
+                // cout << endl;
+        }
+        if(linha == primeiraLinha){ // se a linha for igual a primeira linha passa para a proxima posição do vetor
+            aux+=1;
         }
     }
+    
+
+    for(int i=0; i<entradas; i++){
+        for(int j=0; j<nLinhas; j++){
+            for(int k=0; k<nLinhas; k++){
+                arq[i].matrizResultado[j][k] = arq[i].matrizTimes[j][arq[i].matrizMachine[j][k]-1];
+                cout << arq[i].matrizResultado[j][k] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
     ifs.close();
     return 0;
 }
