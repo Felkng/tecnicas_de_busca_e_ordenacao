ea639432cd4d732c62047c988324342f57031a10
Felkng <felipersf374@gmail.com>
2023-10-05 13:26:03 -0300

    Slice vectors

--- projeto 1/teste.cpp
+++ projeto 1/teste.cpp
@@ -65,10 +65,7 @@ int main() {
     // istringstream iss;
     while(getline(file,line,'\t')){
         if(i%8==0){
-            // cout << "linha: " << line << endl;
             pair<string,string> separator = splitString(line);
-            // cout << "separador 1: " << separator.first << endl;
-            // cout << "separador 2: " << separator.second << endl;
             content.push_back(separator.first);
             if(separator.second != "")
                 content.push_back(separator.second);
@@ -77,41 +74,16 @@ int main() {
         }
         i++;
 
-        // // iss.str("");
-        // // iss.clear();
-        // // iss.str(line);
-        // istringstream iss(line);
-        // // iss >> movie.tconst >> movie.titleType >> movie.primaryTitle >> movie.originalTitle >> movie.isAdult >> movie.startYear >> movie.endYear >> movie.duration >> movie.genres;
-        // // movieTable.push_back(movie);
     }
-    // while (getline(file, line)) {
-    //     regex whiteSpace(" ");
-    //     regex barraN("\\\\N");
-    //     regex igualin("=");
-        
-    //     // write the results to an output iterator
-    //     line = regex_replace(line, whiteSpace, "=");
-    //     line = regex_replace(line, barraN, "-1");
-    //     // iss.getline(line);
-    //     // iss(line);
 
-    //     if((iss >> movie.tconst >> movie.titleType >> movie.primaryTitle >> movie.originalTitle >> movie.isAdult >>  movie.startYear >> movie.endYear >> movie.duration >> movie.genres)){
-    //         movie.primaryTitle = regex_replace(movie.primaryTitle, igualin, " ");
-    //         movie.originalTitle = regex_replace(movie.originalTitle, igualin, " ");
-    //         movieTable.push_back(movie);
-    //     } else {
-    //         cerr << "Failed to parse line: " << line << i << endl;
-
-    //     }
-    // }
-    // map<string,string> title_types;
     file.close();
-    map<string,set<string>> title_types_map;
-    map<string,set<string>> genres_map;
-    map<int,set<string>> startYear_map;
-    for(int i=0;i<content.size();i+=9){
+    vector<string>TitleType;
+    vector<string> genres;
+    vector<int> startYear;
+    for(int i=0;i<396;i+=9){
         movie.tconst = content[i];
         movie.titleType = content[i+1];
+        TitleType.push_back(movie.titleType);
         movie.primaryTitle = content[i+2];
         movie.originalTitle = content[i+3];
         if(content[i+4] == "\\N")
@@ -120,6 +92,7 @@ int main() {
         if(content[i+5] == "\\N")
             content[i+5] = "-1";
         movie.startYear = stoi(content[i+5]);
+        startYear.push_back(movie.startYear);
         if(content[i+6] == "\\N")
             content[i+6] = "-1";
         movie.endYear = stoi(content[i+6]);
@@ -127,40 +100,29 @@ int main() {
             content[i+7] = "-1";
         movie.duration = stoi(content[i+7]);
         movie.genres = content[i+8];
-        title_types_map[movie.titleType].insert(movie.tconst);
-        genres_map[movie.genres].insert(movie.tconst);
-        startYear_map[movie.startYear].insert(movie.tconst);
+        genres.push_back(movie.genres);
         movieTable.push_back(movie);
     }
     content.clear();
+    sort(TitleType.begin(), TitleType.end());
+    sort(genres.begin(), genres.end());
+    sort(startYear.begin(), startYear.end());
+    string val = "short";
 
-    // set<string> title_types_map_union;
-    // set<string> genres_map_union;
+    // Encontrar a primeira ocorrência usando find
+    auto first = find(TitleType.begin(), TitleType.end(), val);
+    auto primeiro = distance(TitleType.begin(), first);
 
-    // for (const auto& entry : title_types_map) {
-    //         cout << entry.first << ": ";
-            
-    //         // Print each element in the vector
-    //         for (const auto& value : entry.second) {
-    //             cout << value << " ";
-    //         }
+    // Encontrar a última ocorrência usando find_end
+    vector <string> a;
+    a.push_back(val);
+    auto end = find_end(TitleType.begin(), TitleType.end(), a.begin(), a.end());
+    auto fim = distance(TitleType.begin(), end);
 
-    //         cout << endl;
-    // }
+    // Imprimir os resultados
+    cout << "Primeira ocorrência de '" << val << "' encontrada no índice " << primeiro << endl;
+    cout << "Última ocorrência de '" << val << "' encontrada no índice " << fim << endl;
 
-    set<string> intersection;
-    // Use set_intersection to find the common elements
-    set_intersection(
-        title_types_map["short"].begin(), title_types_map["short"].end(),
-        genres_map["Drama,History,Short"].begin(), genres_map["Drama,History,Short"].end(),
-        inserter(intersection, intersection.begin())
-    );
-
-    // Print the intersection
-    cout << "Intersection: ";
-    for (string value : intersection) {
-        cout << value << " ";
-    }
     cout << endl;
 
     return 0;
